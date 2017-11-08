53587f1c208ecbf560c31bf56a85d44365690406
dllo <13252984305@163.com>
2017-11-08 17:20:56 +0800

    构建Spring项目框架

---
+++ src/main/java/com/lanou/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.lanou.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dllo on 17/11/8.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/login")
+    public String login(){return "login";}
+}
